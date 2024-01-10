f8be57bede033bb2a5cbe9fe3e35c69d3faf8c64
sanjeevz3009 <sanjeevz1717@gmail.com>
2024-01-10 21:40:04 +0000

    Comments and docstrings added

--- UP2002235_part_3.py
+++ UP2002235_part_3.py
@@ -1,12 +1,17 @@
+# Importing numpy to be used for numerical operations. i.e Arrays and Matrices
 import numpy as np
-
+# Importing numpy typing for type hinting purposes
+import numpy.typing as npt
 
 class MultiLayerNN(object):
-    """_summary_"""
+    """
+    Represents a simple neural network.
+    """
 
     def __init__(self):
         """
         Initialiases the neural network with the weights and biases.
+        It has 2 input neurons, 1 hidden layer with 2 neurons and 1 output neuron.
         """
         # self.weight_input_hidden and self.weight_hidden_output are weight
         # matrices connecting the input to the hidden layer and the hidden layer
@@ -18,25 +23,32 @@ class MultiLayerNN(object):
         self.weight_hidden_output = np.random.rand(2, 1)
         self.bias_output = np.zeros((1, 1))
 
-    def sigmoid(self, x):
+    def sigmoid(self, x: npt.NDArray):
         """
-        Sigmoid activation function
+        Sigmoid activation function.
+        Used in the network's forward and backward passes.
 
-        :param x: _description_
-        :type x: _type_
-        :return: _description_
-        :rtype: _type_
+        :param x: The sigmoid function takes any real-valued number x and 
+        squashes it into the range (0, 1). The output is always between 0 and 1
+        :type x: npt.NDArray
+        :return: The result of this computation, which is a 
+        value between 0 and 1. The output is the activation of a neuron after
+        applying the sigmoid function to its input
+        :rtype: NDArray
         """
+        print(type( 1.0 / (1 + np.exp(-x))))
         return 1.0 / (1 + np.exp(-x))
 
-    def sigmoid_derivative(self, x):
+    def sigmoid_derivative(self, x: npt.NDArray):
         """
-        Calculates the sigmoid derivative
+        Calculates the sigmoid derivative.
+        Used in the network's forward and backward passes.
 
-        :param x: _description_
-        :type x: _type_
-        :return: _description_
-        :rtype: _type_
+        :param x: This is the input to the sigmoid derivative function
+        :type x: : npt.NDArray
+        :return: The function returns the result of the derivative of the
+        sigmoid function applied element-wise to the input array x
+        :rtype: npt.NDArray
         """
         return x * (1.0 - x)
 
@@ -49,17 +61,30 @@ class MultiLayerNN(object):
         Then, it calculates the inputs to the output layer (self.output_input) and the
         final outputs (self.output) using the sigmoid activation function.
 
-        :param x: _description_
+        :param x: This is the input data to the neural network. It represents the features or
+        values that you want the neural network to process
         :type x: _type_
-        :return: _description_
+        :return: The function returns the output of the neural network, which represents the 
+        predicted result after the forward pass
         :rtype: _type_
         """
         # Forward pass through the network
+        # Hidden Layer Input Calculation
+        # This calculates the weighted sum of the input (x) to the hidden layer using the weight matrix self.weight_input_hidden
+        # and adds the bias term self.bias_hidden
         self.hidden_input = np.dot(x, self.weight_input_hidden) + self.bias_hidden
+        # Hidden Layer Output Calculation (Activation)
+        # Applies the sigmoid activation function (self.sigmoid) to the hidden layer input, resulting in the output (self.hidden_output)
         self.hidden_output = self.sigmoid(self.hidden_input)
+        # Output Layer Input Calculation
+        # Calculates the weighted sum of the hidden layer output to obtain the input to the output layer
+        # It also includes the bias term
         self.output_input = (
             np.dot(self.hidden_output, self.weight_hidden_output) + self.bias_output
         )
+        # Output Layer Output Calculation (Activation)
+        # Applies the sigmoid activation function to the output layer input, resulting in the final output 
+        # of the neural network (self.output)
         self.output = self.sigmoid(self.output_input)
         return self.output
 
@@ -71,24 +96,36 @@ class MultiLayerNN(object):
         Gradients are calculated for both output and hidden layers.
         Weights and biases are updated based on the calculated gradients and the learning rate.
 
-        :param input_data: _description_
+        :param input_data: The input data that was fed into the network during the forward pass
         :type input_data: _type_
-        :param target: _description_
+        :param target: The target or true output corresponding to the input data
         :type target: _type_
-        :param learning_rate: _description_
+        :param learning_rate: The learning rate hyperparameter, controlling the size of weight updates
         :type learning_rate: _type_
         """
         # Back propagate to update the weights using back propagation
+        # Output Layer Error Calculation
+        # Calculates the error at the output layer by taking the difference between the target output and the actual output
         output_error = target - self.output
+        # Sigmoid Derivatives
+        # Calculates the derivatives of the sigmoid activation function for the output and hidden layers
+        # These derivatives are needed for the back propagation calculations
         sigmoid_derivative_output = self.sigmoid_derivative(self.output)
         sigmoid_derivative_hidden = self.sigmoid_derivative(self.hidden_output)
 
+        # Output and Hidden Layer Deltas
+        # Computes the deltas for the output and hidden layers
+        # These deltas represent the contribution of each layer to the overall error
         output_delta = output_error * sigmoid_derivative_output
         hidden_delta = (
             np.dot(output_delta, self.weight_hidden_output.T)
             * sigmoid_derivative_hidden
         )
 
+        # Weight and Bias Updates
+        # Updates the weights and biases based on the calculated deltas and the learning rate
+        # This step is crucial for minimising the error in the network by adjusting the parameters
+        # in the direction that reduces the error.
         self.weight_hidden_output += learning_rate * np.outer(
             self.hidden_output, output_delta
         )
@@ -105,21 +142,30 @@ class MultiLayerNN(object):
         updates the parameters.
         The mean squared error (MSE) is calculated for each epoch and printed too.
 
-        :param input_data: _description_
+        :param input_data: The input data used for training
         :type input_data: _type_
-        :param target_data: _description_
+        :param target_data: The target or true output corresponding to the input data
         :type target_data: _type_
-        :param epochs: _description_
+        :param epochs: The number of times to iterate through the entire training dataset
         :type epochs: _type_
-        :param learning_rate: _description_
+        :param learning_rate: The learning rate hyperparameter, controlling the size of weight updates
         :type learning_rate: _type_
         """
+        # Training Loop
+        # Iterates over the specified number of epochs
         for epoch in range(epochs):
+            # Initialises the total error for the current epoch to 0
             total_error = 0
+            # Iterates through each training example in the dataset
             for input, target_output in zip(input_data, target_data):
+                # Performs a forward pass to obtain the output of the neural network for the given input
                 output = self.feed_forward(input)
+                # Performs the back propagation to update the weights and biases based on the error
                 self.back_propagate(input, target_output, learning_rate)
+                #  Calculates and accumulates the mean squared error for the current training example
                 total_error += np.mean((target_output - output) ** 2)
+            # Prints the mean squared error for the current epoch
+            # This provides information on how well the neural network is performing on the training data
             print(
                 f"Epoch {epoch + 1}/{epochs}, Mean Squared Error: {total_error / len(input_data)}"
             )
